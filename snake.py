afb9e7c31dc87db6b4b08b88ecd11cba95695814
Boris Gubanov <44787162+boris-gu@users.noreply.github.com>
2021-05-30 14:07:37 +0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -7,11 +7,6 @@ import time
 import subjects
 import render
 
-directions = ['right', 'up', 'left', 'down']
-lock = threading.Lock()
-if os.name == 'posix':
-    os.system('stty -echo')
-
 
 def control(e):
     if e.event_type == 'down' and (e.name in directions) and e.name != snake.direction:
@@ -21,45 +16,51 @@ def control(e):
                 snake.direction = e.name
 
 
-try:
-    render.warning()
-    time.sleep(3)
-    while True:
-        with lock:
-            print('\033[?25l', end='')
-        render.cover()
-        render.frame()
+if __name__ == '__main__':
+    directions = ['right', 'up', 'left', 'down']
+    lock = threading.Lock()
+    if os.name == 'posix':
+        os.system('stty -echo')
 
-        snake = subjects.Snake()
-        snake.render()
-        food = subjects.Food(snake)
-        food.render()
-        score = 0
-        render.score()
-
-        keyboard.hook(control)
-        # ГЕЙМПЛЕЙ
+    try:
+        render.warning()
+        time.sleep(3)
         while True:
-            time.sleep(0.15)
             with lock:
-                snake.move()
-            if snake.hit():
-                break
-            snake.render()
-            if snake.head == food:
-                snake.add_cell()
-                food = subjects.Food(snake)
-                food.render()
-                score += 1
-                render.score(score)
-            snake.update_direction()
-        time.sleep(2)
-        render.clear()
-        render.game_over(score)
-        time.sleep(5)
-except KeyboardInterrupt:
-    pass
+                print('\033[?25l', end='')
+            render.cover()
+            render.frame()
 
-print('\033[?25h', end='')
-if os.name == 'posix':
-    os.system('stty -echo')
+            snake = subjects.Snake()
+            snake.render()
+            food = subjects.Food(snake)
+            food.render()
+            score = 0
+            render.score()
+
+            keyboard.hook(control)
+            # ГЕЙМПЛЕЙ
+            while True:
+                time.sleep(0.15)
+                with lock:
+                    snake.move()
+                if snake.hit():
+                    break
+                snake.render()
+                if snake.head == food:
+                    snake.add_cell()
+                    food = subjects.Food(snake)
+                    food.render()
+                    score += 1
+                    render.score(score)
+                snake.update_direction()
+            time.sleep(2)
+            render.clear()
+            render.game_over(score)
+            time.sleep(5)
+    except KeyboardInterrupt:
+        pass
+
+    print('\033[?25h', end='')
+    if os.name == 'posix':
+        os.system('stty -echo')
